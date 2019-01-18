ac50635f822f14cf19a2f64fa3127040115f2fc9
faiza2902 <46339651+faiza2902@users.noreply.github.com>
2019-01-18 12:36:11 +0530

    Add files via upload

---
+++ module-1--assign/app.js
@@ -0,0 +1,23 @@
+angular.module=('LunchChecker', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject=['$scope'];
+function LunchCheckController($scope){
+    $scope.message="";
+    $scope.listItems="";
+    $scope.calculator= function () {
+        
+        var noOfItems = 0;
+        noOfItems=$scope.listItems.split(",");
+        console.log(noOfItems.length);
+        if(noOfItems=="") {
+            $scope.message="please enter data first";
+        } else if(noOfItems.length<=3) {
+            $scope.message="enjoy!";
+        } else if(noOfItems.length>3) {
+            $scope.message="too much!";
+        }
+    };
+    
+    
+}
